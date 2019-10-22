11b2b18f3f6be4be217e5c6193271c2374ac7bb7
marcos <marcos@marcos>
2019-10-21 21:00:48 -0300

    Configuração inicial do projeto, utilizando Spring Initializr

---
+++ src/main/java/br/com/marcos/dadosgeograficosbrasil/DadosGeograficosBrasilService/DadosGeograficosBrasilService/DadosGeograficosBrasilServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.dadosgeograficosbrasil.DadosGeograficosBrasilService.DadosGeograficosBrasilService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadosGeograficosBrasilServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadosGeograficosBrasilServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/dadosgeograficosbrasil/DadosGeograficosBrasilService/DadosGeograficosBrasilService/DadosGeograficosBrasilServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.dadosgeograficosbrasil.DadosGeograficosBrasilService.DadosGeograficosBrasilService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosGeograficosBrasilServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
